518e7c7ffa44752808313e57666a77327cdb6575
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-31 21:49:44 +0530

    Nested and Anonimous Function.

---
+++ app.js
@@ -0,0 +1,15 @@
+function func(Email,Password){
+	 var y=10;
+	  var shahnawaz=function(){
+             
+            if(Email=="Prolog"){
+            	return "Welcome to Prolog";
+            }
+	  }
+	  return shahnawaz;
+}
+var v=func("Prolog","prolog786");
+var v2=v();
+alert(v2);
+
+
